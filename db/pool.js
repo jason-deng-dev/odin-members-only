6af0fae14075e3af42e2aaccd6de86ab9c1922d0
Jason <jason13.deng@gmail.com>
2026-03-12 13:40:53 +0900

    feat: switch env url name

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,6 @@ const { Pool } = require("pg");
 require('dotenv').config()
 
 module.exports = new Pool({
-  connectionString: process.env.INTERNAL_DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
